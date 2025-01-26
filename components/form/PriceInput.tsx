d7250b4605ef18f5827e48d816dbbba52636c7f9
Zülal Cengiz <zc.swe@gmail.com>
2025-01-26 16:10:18 +0300

    Refactor PriceInput component to use a constant for the field name

--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -2,22 +2,20 @@ import { Prisma } from "@prisma/client";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 
-const name = Prisma.PropertyScalarFieldEnum.price;
-
 type FormInputNumberProps = {
   defaultValue?: number;
 };
 
 function PriceInput({ defaultValue }: FormInputNumberProps) {
+  const name = "price";
   return (
     <div className="mb-2">
-      <Label htmlFor="price">Price ($)</Label>
+      <Label htmlFor={name}>Price ($)</Label>
       <Input
         id={name}
         name={name}
         type="number"
         min={0}
-        placeholder="Enter the price"
         defaultValue={defaultValue || 100}
         required
       />
