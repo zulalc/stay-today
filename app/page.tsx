eaeab6a539c997e0248790d9419657ef9ba91a1c
Zülal Cengiz <zc.swe@gmail.com>
2025-01-27 22:15:25 +0300

    Refactor HomePage to utilize useSearchParams for category and search handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-import CategoriesList from "@/components/home/CategoriesList";
+"use client";
 
-function HomePage({
-  searchParams,
-}: {
-  searchParams: { category?: string; search?: string };
-}) {
+import CategoriesList from "@/components/home/CategoriesList";
+import { useSearchParams } from "next/navigation";
+
+function HomePage() {
+  const searchParams = useSearchParams();
+  const category = searchParams.get("category") ?? undefined;
+  const search = searchParams.get("search") ?? undefined;
   return (
     <section>
-      <CategoriesList
-        category={searchParams?.category}
-        search={searchParams?.search}
-      />
+      <CategoriesList category={category} search={search} />
     </section>
   );
 }
