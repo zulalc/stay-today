a7c61961194c79460f956a2ca974cf184c9904f6
Zülal Cengiz <zc.swe@gmail.com>
2025-02-02 21:44:38 +0300

    Add Loading component with skeleton placeholders for async data loading

---
+++ components/card/Loading.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Skeleton } from "../ui/skeleton";
+
+function Loading() {
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <LoadingCard />
+      <LoadingCard />
+      <LoadingCard />
+      <LoadingCard />
+    </section>
+  );
+}
+
+export default Loading;
+
+export function LoadingCard() {
+  return (
+    <div>
+      <Skeleton className="h-[300px] rounded-md" />
+      <Skeleton className="h-4 w-3/4" />
+      <Skeleton className="h-4 w-1/2" />
+    </div>
+  );
+}
