34bbfd7887cb294406df1fb7fcc690222351fff3
Zülal Cengiz <zc.swe@gmail.com>
2025-02-02 21:45:20 +0300

    Add Loading component and wrap PropertiesContainer in Suspense for async data handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import Loading from "@/components/card/Loading";
 import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 function HomePage() {
   const searchParams = useSearchParams();
@@ -10,6 +13,8 @@ function HomePage() {
   return (
     <section>
       <CategoriesList category={category} search={search} />
+      <Suspense fallback={<Loading />} />
+      <PropertiesContainer category={category} search={search} />
     </section>
   );
 }
