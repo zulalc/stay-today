538773ee5547a19ca1dbf0385ffd4429fd53b24a
Zülal Cengiz <zc.swe@gmail.com>
2025-02-02 21:43:47 +0300

    Add CountryDetails component for displaying country information

---
+++ components/card/CountryDetails.tsx
@@ -0,0 +1,16 @@
+import { findCountryByCode } from "@/utils/countries";
+
+function CountryDetails({ countryCode }: { countryCode: string }) {
+  const country = findCountryByCode(countryCode);
+  const countryName =
+    country!.name.length > 20
+      ? country!.name.substring(0, 20) + "..."
+      : country!.name;
+  return (
+    <span className="flex justify-between items-center gap-2 text-sm">
+      {country?.flag} {countryName}
+    </span>
+  );
+}
+
+export default CountryDetails;
