5bb4fc018475f663b6ed1bb5caa8668e7646a830
Zülal Cengiz <zc.swe@gmail.com>
2025-02-02 21:44:02 +0300

    Add PropertyRating component for displaying property ratings and review counts

---
+++ components/card/PropertyRating.tsx
@@ -0,0 +1,23 @@
+import { Star } from "lucide-react";
+
+async function PropertyRating({
+  propertyId,
+  inPage,
+}: {
+  propertyId: string;
+  inPage: boolean;
+}) {
+  const rating = 3.5;
+  const totalReviews = 100;
+  const className = `flex gap-1 items-center ${inPage ? "text-md" : "text-xs"}`;
+  const reviewCount = totalReviews > 1 ? "reviews" : "review";
+  const reviewValue = `(${totalReviews} ${inPage ? reviewCount : ""})`;
+  return (
+    <span className={className}>
+      <Star className="w-3 h-3" />
+      {rating} {reviewValue}
+    </span>
+  );
+}
+
+export default PropertyRating;
