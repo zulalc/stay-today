0bf28c9ec91df5f13752bcb5fa573abc0fa9b839
Zülal Cengiz <zc.swe@gmail.com>
2025-02-07 16:24:23 +0300

    Render PropertyCard for each property in PropertyList component

--- components/home/PropertyList.tsx
+++ components/home/PropertyList.tsx
@@ -1,10 +1,11 @@
 import { PropertyProps } from "@/utils/types";
+import PropertyCard from "../card/PropertyCard";
 
 function PropertyList({ properties }: { properties: PropertyProps[] }) {
   return (
     <section className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-4 gap-8">
       {properties.map((property) => {
-        return <></>;
+        return <PropertyCard key={property.id} property={property} />;
       })}
     </section>
   );
