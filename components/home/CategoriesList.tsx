9d17b0bdc5814a6d05e3ad0077573b6cd3e8bab5
Zülal Cengiz <zc.swe@gmail.com>
2025-02-07 18:05:08 +0300

    Enhance CategoriesList component with horizontal scroll and improved layout

--- components/home/CategoriesList.tsx
+++ components/home/CategoriesList.tsx
@@ -1,5 +1,5 @@
 import { categories } from "@/utils/categories";
-import { ScrollArea } from "../ui/scroll-area";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 import Link from "next/link";
 
 function CategoriesList({
@@ -12,9 +12,10 @@ function CategoriesList({
   const searchWord = search ? `&search=${search}` : "";
 
   return (
-    <section>
-      <ScrollArea className="p-6">
-        <div className="flex gap-x-4">
+    <section className="w-full max-w-screen-md mx-auto">
+      <ScrollArea className="p-4 w-full overflow-x-auto">
+        <ScrollBar orientation="horizontal" />
+        <div className="flex gap-x-4 w-max">
           {categories.map((cat) => {
             const isActive = cat.label === category;
             return (
@@ -23,12 +24,14 @@ function CategoriesList({
                 href={`/?category=${cat.label}${searchWord}`}
               >
                 <article
-                  className={`p-4 flex flex-col items-center cursor-pointer duration-300 hover:text-primary w-[100px] ${
-                    isActive ? "text-primary" : ""
+                  className={`p-4 flex flex-col items-center text-center cursor-pointer duration-300 hover:text-primary w-[100px] ${
+                    isActive ? "text-primary " : ""
                   }`}
                 >
                   <cat.icon className="w-8 h-8" />
-                  <span className="mt-2 text-sm capitalize">{cat.label}</span>
+                  <span className="mt-2 text-sm text-center capitalize">
+                    {cat.label}
+                  </span>
                 </article>
               </Link>
             );
