05fe20e3217d2f9d08986c62d5606e5493ad8ddc
Zülal Cengiz <zc.swe@gmail.com>
2025-02-07 16:24:10 +0300

    Refactor PropertiesContainer to use hooks for async data fetching and add loading state

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -2,26 +2,50 @@ import { fetchProperties } from "@/utils/actions";
 import { PropertyProps } from "@/utils/types";
 import EmptyList from "./EmptyList";
 import PropertyList from "./PropertyList";
+import { useEffect, useState } from "react";
+import Loading from "../card/Loading";
 
-async function PropertiesContainer({
+function PropertiesContainer({
   search,
   category,
 }: {
   category?: string;
   search?: string;
 }) {
-  const properties: PropertyProps[] = await fetchProperties({
-    category,
-    search,
-  });
+  const [properties, setProperties] = useState<PropertyProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let isMounted = true; // Prevents state updates if unmounted
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchProperties({ category, search });
+        if (isMounted) setProperties(data);
+      } catch (error) {
+        console.error("Error fetching properties:", error);
+      } finally {
+        if (isMounted) setLoading(false);
+      }
+    };
+
+    fetchData();
+    return () => {
+      isMounted = false;
+    };
+  }, [category, search]);
+
+  if (loading) return <Loading />;
 
   if (properties.length === 0) {
     return (
-      <EmptyList
-        heading="No properties found"
-        message="Please try again later"
-        btnText="Clear filters"
-      />
+      <div className="flex items-center justify-center ">
+        <EmptyList
+          heading="No properties found"
+          message="Please try again later"
+          btnText="Clear filters"
+        />
+      </div>
     );
   }
 
