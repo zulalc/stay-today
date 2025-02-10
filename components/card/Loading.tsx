091769c85315f35b604bda633de522c59230a22c
Zülal Cengiz <zc.swe@gmail.com>
2025-02-10 18:13:01 +0300

    Remove unnecessary skeleton elements from LoadingCard component

--- components/card/Loading.tsx
+++ components/card/Loading.tsx
@@ -18,8 +18,6 @@ export function LoadingCard() {
   return (
     <div>
       <Skeleton className="h-[300px] rounded-md" />
-      <Skeleton className="h-4 w-3/4" />
-      <Skeleton className="h-4 w-1/2" />
     </div>
   );
 }
