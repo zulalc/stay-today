dfd9f9811252c54e8b3b918d4d8b1ecde8dd2e94
Zülal Cengiz <zc.swe@gmail.com>
2025-02-10 18:13:19 +0300

    Implement debounced search functionality in NavbarSearch component

--- components/navbar/NavbarSearch.tsx
+++ components/navbar/NavbarSearch.tsx
@@ -1,11 +1,44 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 function NavbarSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname(); //get current URL path
+  const { replace } = useRouter(); //update URL without full page reload
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString || ""
+  );
+
+  //DebouncedCallback: Delays function execution to prevent excessive updates when typing.
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
   return (
     <Input
-      type="text"
+      type="search"
       placeholder="Find a place to stay..."
       className="max-w-xs dark:bg-muted rounded-full px-4"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 }
