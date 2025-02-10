4132bcb4e7f5813fd4be6f13e2edc537afa549c4
Zülal Cengiz <zc.swe@gmail.com>
2025-02-10 18:13:09 +0300

    Add CardSignInButton and CardSubmitButton components for favorite functionality

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -2,6 +2,8 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { SignInButton } from "@clerk/nextjs";
+import { BsHeart, BsHeartFill } from "react-icons/bs";
 
 type btnSize = "default" | "sm" | "lg";
 
@@ -30,3 +32,40 @@ export function SubmitButton({
     </Button>
   );
 }
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <BsHeart />
+      </Button>
+    </SignInButton>
+  );
+};
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="outline"
+      className="p-2 cursor-pointer"
+    >
+      {pending ? (
+        <ReloadIcon className="h-4 w-4 animate-spin" />
+      ) : isFavorite ? (
+        <BsHeartFill />
+      ) : (
+        <BsHeart />
+      )}
+    </Button>
+  );
+};
