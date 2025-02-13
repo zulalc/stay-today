011e2739fe92cbde4a4e10f92ffc995d2a44b80b
Zülal Cengiz <zc.swe@gmail.com>
2025-02-13 21:40:05 +0300

    Add PropertyDetails component and formatQuantity utility function

---
+++ app/properties/PropertyDetails.tsx
@@ -0,0 +1,21 @@
+import { formatQuantity } from "@/utils/format";
+import { PropertyDetailsProps } from "@/utils/types";
+
+function PropertyDetails({
+  propertyDetails,
+}: {
+  propertyDetails: PropertyDetailsProps;
+}) {
+  return (
+    <p className="text-md font-light ">
+      <span>
+        {formatQuantity(propertyDetails.bedrooms, "bedroom")} &middot;{" "}
+      </span>
+      <span>{formatQuantity(propertyDetails.bathrooms, "bath")} &middot; </span>
+      <span>{formatQuantity(propertyDetails.guests, "guest")} &middot; </span>
+      <span>{formatQuantity(propertyDetails.beds, "bed")}</span>
+    </p>
+  );
+}
+
+export default PropertyDetails;
