58a0eab99b44e41c23e7ecec2b44218fd40b10ef
Zülal Cengiz <zc.swe@gmail.com>
2025-02-13 15:51:51 +0300

    Add PropertyRating and ImageContainer components to PropertyDetailsPage

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import FavToggleButton from "@/components/card/FavToggleButton";
+import PropertyRating from "@/components/card/PropertyRating";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import ImageContainer from "@/components/properties/ImageContainer";
+import ShareButton from "@/components/properties/ShareButton";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { PropertyDetailsProps } from "@/utils/types";
 import { useParams, useRouter } from "next/navigation";
@@ -35,9 +38,28 @@ function PropertyDetailsPage() {
       <header className="flex justify-between items-center mt-4">
         <h1 className="text-4xl font-bold">{propertyDetails.tagline}</h1>
         <div className="flex items-center gap-x-4">
+          <ShareButton
+            name={propertyDetails.name}
+            propertyId={propertyDetails.id}
+          />
           <FavToggleButton propertyId={id} />
         </div>
       </header>
+      <ImageContainer
+        image={propertyDetails.image}
+        name={propertyDetails.name}
+      />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{propertyDetails.name}</h1>
+            <PropertyRating inPage propertyId={id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          {/*calendar*/}
+        </div>
+      </section>
     </section>
   );
 }
