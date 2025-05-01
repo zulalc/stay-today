acff2fe2cd2754c5595504d5e2c5ca612fc14c8e
Zülal Cengiz <zc.swe@gmail.com>
2025-05-01 18:38:16 +0300

    Refactor Navbar component for improved layout and responsiveness

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,10 +6,20 @@ import Logo from "./Logo";
 function Navbar() {
   return (
     <nav className="border-b">
-      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-5 py-8">
-        <Logo />
-        <NavbarSearch />
-        <div className="flex gap-4 items-center">
+      <div className="container flex flex-col gap-4 py-4 sm:flex-row sm:justify-between sm:items-center">
+        <div className="flex justify-between items-center">
+          <Logo />
+          {/* Shown on mobile */}
+          <div className="flex items-center gap-3 sm:hidden">
+            <DarkMode />
+            <LinksDropdown />
+          </div>
+        </div>
+        <div className="w-full sm:w-auto">
+          <NavbarSearch />
+        </div>
+        {/* Shown on desktop */}
+        <div className="hidden sm:flex gap-4 items-center">
           <DarkMode />
           <LinksDropdown />
         </div>
