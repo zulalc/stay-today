da169e7c37065b94ff937d9cf1a3822e4b9f6535
Zülal Cengiz <zc.swe@gmail.com>
2025-05-01 18:46:20 +0300

    Refactor Loading component to dynamically render multiple LoadingCard instances and improve layout structure

--- components/card/Loading.tsx
+++ components/card/Loading.tsx
@@ -3,11 +3,10 @@ import { Skeleton } from "../ui/skeleton";
 
 function Loading() {
   return (
-    <section className="mt-4 gap-8 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <LoadingCard />
-      <LoadingCard />
-      <LoadingCard />
-      <LoadingCard />
+    <section className="mt-4 grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {Array.from({ length: 8 }).map((_, i) => (
+        <LoadingCard key={i} />
+      ))}
     </section>
   );
 }
@@ -16,8 +15,24 @@ export default Loading;
 
 export function LoadingCard() {
   return (
-    <div>
-      <Skeleton className="h-[300px] rounded-md" />
+    <div className="space-y-2">
+      {/* Image */}
+      <Skeleton className="h-[280px] rounded-lg" />
+
+      {/* country + rating */}
+      <div className="flex justify-between items-center">
+        <Skeleton className="h-4 w-20 rounded" />
+        <Skeleton className="h-4 w-12 rounded" />
+      </div>
+
+      {/* Name/title */}
+      <Skeleton className="h-4 w-32 rounded" />
+
+      {/* Tagline + price */}
+      <div className="flex justify-between items-center">
+        <Skeleton className="h-4 w-40 rounded" />
+        <Skeleton className="h-4 w-16 rounded" />
+      </div>
     </div>
   );
 }
