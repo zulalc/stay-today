c0f8acea5a7860753d22bc9d57bf1f16ca48f6ba
Zülal Cengiz <zc.swe@gmail.com>
2025-05-04 16:38:38 +0300

    Loading for property details

--- app/properties/[id]/loading.tsx
+++ app/properties/[id]/loading.tsx
@@ -1,7 +1,20 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-function loading() {
-  return <Skeleton className="h-[300px] md:h-[500px] w-full rounded" />;
+function Loading() {
+  return (
+    <div className="space-y-4">
+      <div className="flex justify-between items-center">
+        <Skeleton className="h-6 w-1/3 rounded" />
+
+        <div className="flex gap-2">
+          <Skeleton className="h-8 w-8 rounded" />
+          <Skeleton className="h-8 w-8 rounded" />
+        </div>
+      </div>
+
+      <Skeleton className="h-[300px] md:h-[500px] w-full rounded" />
+    </div>
+  );
 }
 
-export default loading;
+export default Loading;
