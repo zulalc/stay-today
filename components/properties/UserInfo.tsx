dd933ff3562e61c66878e169914fbff8f8e33648
Zülal Cengiz <zc.swe@gmail.com>
2025-05-04 17:01:50 +0300

    Refactor UserInfo component layout for improved responsiveness and styling

--- components/properties/UserInfo.tsx
+++ components/properties/UserInfo.tsx
@@ -4,18 +4,16 @@ import React from "react";
 
 function UserInfo({ profile: { profileImage, firstName } }: UserInfoProps) {
   return (
-    <div className="flex items-center gap-4">
-      <div className="flex flex-col text-end">
-        <p className="text-sm">
+    <div className="flex flex-col sm:flex-row sm:items-center sm:justify-end gap-4 w-full sm:w-auto">
+      <div className="flex flex-col text-start sm:text-end flex-1">
+        <p className="text-sm text-gray-800 dark:text-white">
           Hosted by <span className="font-bold">{firstName}</span>
         </p>
 
-        <div className="flex gap-2">
-          <p className="text-sm text-muted-foreground font-medium">Superhost</p>
+        <div className="flex gap-2 text-sm text-muted-foreground">
+          <p className="font-medium">Superhost</p>
           &middot;
-          <p className="text-sm text-muted-foreground font-light">
-            Joined in 2020
-          </p>
+          <p className="font-light">Joined in 2020</p>
         </div>
       </div>
       <Image
@@ -23,7 +21,7 @@ function UserInfo({ profile: { profileImage, firstName } }: UserInfoProps) {
         alt={firstName}
         width={50}
         height={50}
-        className="rounded-full w-12 h-12 object-cover"
+        className="rounded-full w-12 h-12 object-cover border"
       />
     </div>
   );
